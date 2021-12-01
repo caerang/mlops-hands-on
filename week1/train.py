69699acc979101d6636be8eb0b361b2673256443
Hyeongdo Lee <mylovercorea@gmail.com>
2021-12-01 16:38:29 +0900

    Add practice up to the 3rd week

---
+++ week1/train.py
@@ -0,0 +1,71 @@
+import torch
+import wandb
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning import LightningModule
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
+
+from data import DataModule
+from model import ColaModel
+
+
+class SamplesVisualisationLogger(pl.Callback):
+    def __init__(self, datamodule) -> None:
+        super().__init__()
+        
+        self.datamodule = datamodule
+        
+    def on_validation_end(self, trainer, pl_module: LightningModule) -> None:
+        val_batch = next(iter(self.datamodule.val_dataloader()))
+        sentences = val_batch["label"]
+        
+        outputs = pl_module(val_batch["input_ids"], val_batch["attention_mask"])
+        preds = torch.argmax(outputs.logits, dim=1)
+        labels = val_batch["label"]
+        
+        df = pd.DataFrame(
+            {"Sentence": sentences, "Label": labels.numpy(), "Predicted": preds.numpy()}
+        )
+        
+        wrong_df = df[df["Label"] != df["Predicted"]]
+        trainer.logger.experiment.log(
+            {
+                "examples": wandb.Table(dataframe=wrong_df, allow_mixed_types=True),
+                "global_step": trainer.global_step,
+            }
+        )
+        
+        
+def main():
+    cola_data = DataModule()
+    cola_model = ColaModel()
+    
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="./models",
+        filename="best-checkpoint.ckpt",
+        monitor="valid/loss",
+        mode="min",
+    )
+    
+    early_stopping_callback = EarlyStopping(
+        monitor="valid/loss", patience=3, verbose=True, mode="min"
+    )
+    
+    wandb_logger = WandbLogger(project="MLOps Basics", entity="caerang")
+    trainer = pl.Trainer(
+        max_epochs=10,
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback, SamplesVisualisationLogger(cola_data), early_stopping_callback],
+        log_every_n_steps=20,
+        deterministic=True,
+        # limit_train_batches=0.25,
+        # limit_val_batches=0.25
+    )
+    trainer.fit(cola_model, cola_data)
+    wandb.finish()
+    
+    
+if __name__ == "__main__":
+    main()
